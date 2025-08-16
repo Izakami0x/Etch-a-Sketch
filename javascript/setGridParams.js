b9052422851ea21e28fc2f61fb5043dc73c3a50f
Izakami <87597619+Izakami0x@users.noreply.github.com>
2025-08-16 15:02:41 +0800

    UPDATE: Added setGridParams.js - Set grid size with a push of a button

--- javascript/setGridParams.js
+++ javascript/setGridParams.js
@@ -1,7 +1,30 @@
-// const btn = document.querySelector("button");
-// btn.addEventListener('click', e => {
-//   let params = prompt("Set number of squares per side for the new grid.", deafultGridSize);
-//   deafultGridSize = parseInt(params);
-//   createGrid(deafultGridSize,deafultGridSize);
-//   // console.log(wrap);
-// })
+const btn = document.querySelector("button");
+
+function res() {
+  const cols = document.querySelectorAll(".col"); // Select all columns
+  cols.forEach(col => col.remove()); // Remove each column
+}
+
+
+function ask() {
+  let params = prompt("Set number of squares per side for the new grid.", deafultGridSize);
+  let param = parseInt(params);
+
+  if (param < 100) {
+  createGrid(param);
+  }
+  else{
+    alert("Number of squares per side must not exceed 100")
+    ask();
+  }
+}
+
+// function isGridValid(number) {
+
+// }
+
+btn.addEventListener('click', e => {
+  res();
+  ask();
+  // isGridValid(value);
+})
